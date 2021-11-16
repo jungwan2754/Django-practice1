8c1b8bbfb9f7a9772acefe8bea98ef88efe69c53
Jun Gwan <jungwan2754@naver.com>
2021-11-16 15:57:35 +0900

    ALLOWD_HOSTS update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9@&1e1pme$%i^2j6-)u^xeu@(-9dr$7@#&u=w1&(+-hy(*ih7s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [3.36.169.155]
 
 
 # Application definition
